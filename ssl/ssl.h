1a036568018b4e0a5d927de1d305063754e078fb
Nicolas Dusart <nicolas@freedelity.be>
2013-03-15 14:08:52 +0100

    add declaration of set_client_verify_sign_cb + update header file in include directory

--- ssl/ssl.h
+++ ssl/ssl.h
@@ -925,6 +925,7 @@ SSL_SESSION *(*SSL_CTX_sess_get_get_cb(SSL_CTX *ctx))(struct ssl_st *ssl, unsign
 void SSL_CTX_set_info_callback(SSL_CTX *ctx, void (*cb)(const SSL *ssl,int type,int val));
 void (*SSL_CTX_get_info_callback(SSL_CTX *ctx))(const SSL *ssl,int type,int val);
 void SSL_CTX_set_client_cert_cb(SSL_CTX *ctx, int (*client_cert_cb)(SSL *ssl, X509 **x509, EVP_PKEY **pkey));
+void SSL_CTX_set_client_verify_sign_cb(SSL_CTX *ctx, int (*cb)(SSL*, const unsigned char*, unsigned int, unsigned char *, unsigned int *));
 int (*SSL_CTX_get_client_cert_cb(SSL_CTX *ctx))(SSL *ssl, X509 **x509, EVP_PKEY **pkey);
 #ifndef OPENSSL_NO_ENGINE
 int SSL_CTX_set_client_cert_engine(SSL_CTX *ctx, ENGINE *e);
